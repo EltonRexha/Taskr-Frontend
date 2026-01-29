2ede48024b23278191e7f70257e975af3279606a
Elton Rexha <elton_rexha@outlook.com>
2026-01-29 21:33:13 +0100

    refactor to using openAPI

--- src/features/projects/hooks/use-projects.ts
+++ src/features/projects/hooks/use-projects.ts
@@ -1,15 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import projectApi from "../api/projects.api";
+import { paths } from "@/api/types";
+
+type ProjectQueryParams = paths["/projects"]["get"]["parameters"]["query"];
 
 export const projectQueryKeys = {
   all: ["projects"] as const,
-  list: (projectName?: string) =>
-    [...projectQueryKeys.all, "list", projectName] as const,
+  list: (stringifiedQuery: string) =>
+    [...projectQueryKeys.all, "list", stringifiedQuery] as const,
 };
 
-export function useProjects({ projectName }: { projectName?: string }) {
+export function useProjects(query: ProjectQueryParams) {
   return useQuery({
-    queryKey: projectQueryKeys.list(projectName),
-    queryFn: () => projectApi.getProjects({ projectName }),
+    queryKey: projectQueryKeys.list(JSON.stringify(query)),
+    queryFn: () => projectApi.getProjects(query),
   });
 }
